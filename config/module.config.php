e7072f6f4551e1618b8c494259c4cf70da82e911
crimsonronin <josh.stuart@zoopcommerce.com>
2014-07-27 12:43:39 +1000

    Moved the error templates to api rather than the store module.

--- config/module.config.php
+++ config/module.config.php
@@ -6,7 +6,20 @@ return [
             'name' => 'rest',
             'route' => '[/:endpoint][/:id]',
             'endpoints' => []
-        ]
+        ],
+        'shard' => [
+            'manifest' => [
+                'commerce' => [
+                    'extension_configs' => [
+                        'extension.accesscontrol' => true,
+                        'extension.softdelete' => true,
+                        'extension.serializer' => true,
+                        'extension.state' => true,
+                        'extension.zone' => true
+                    ],
+                ],
+            ],
+        ],
     ],
     'router' => [
         'prototypes' => [
@@ -42,6 +55,21 @@ return [
         ],
         'factories' => [
             'Router' => 'Zoop\Api\Service\RouterFactory',
+        ],
+    ],
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => array(
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => [
+            __DIR__ . '/../view',
         ],
     ],
 ];
