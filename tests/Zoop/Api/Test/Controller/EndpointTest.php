e43aa1d90d75d616a8767917a7b000e6d444a92d
crimsonronin <josh.stuart@zoopcommerce.com>
2014-08-26 15:32:42 +1000

    cs fixes

--- tests/Zoop/Api/Test/Controller/EndpointTest.php
+++ tests/Zoop/Api/Test/Controller/EndpointTest.php
@@ -81,7 +81,7 @@ class EndpointTest extends AbstractTest
 
         $content = $response->getContent();
         $this->assertEmpty($content);
-    } 
+    }
     
     protected function createTestData()
     {
